d7c7e002b7d8c232bfec1e97edbf8871eb35e870
Aceticia <liuchris34@gmail.com>
2022-03-08 10:59:27 -0500

    Running

--- optuna_script.py
+++ optuna_script.py
@@ -1,20 +1,19 @@
 import optuna
+from optuna.integration import PyTorchLightningPruningCallback
 
-import torch.optim
-from torch.utils.data import random_split
+from torch.utils.data import random_split, DataLoader
 
 import albumentations as A
-from torchvision.datasets import CIFAR10
+from data_augmentation.dataset import Cifar10SearchDataset
 
-"""
-TODOs:
-- Add early stopping
-- Add importance sampling
-- Add Learning rate decay
-"""
+import pytorch_lightning as pl
+from pytorch_lightning.loggers import WandbLogger
+
+from project1_model import ResNet
+
 
 # Generic hyperparameters
-min_layers = 3     # Minimum layers
+min_layers = 4     # Minimum layers
 max_layers = 7     # Maximum layers
 min_b = 2          # Minimum blocks per layer
 max_b = 10         # Maximum blocks per layer
@@ -28,38 +27,27 @@ min_avg_w = 2      # Minimum average pooling kernel size (1+2x)
 max_avg_w = 9      # Minimum average pooling kernel size (1+2x)
 
 # Regularizer
-max_weight_decay = 0
-min_weight_decay = 1e-2
+max_weight_decay = 1e-2
+min_weight_decay = 1e-6
+
 
 # Learning related
 max_lr = 1e-2
 min_lr = 1e-6
-max_beta_1 = 1-1e-10
-min_beta_1 = 0.5
-max_beta_2 = 1-1e-10
-min_beta_2 = 0.5
-opt = ['AdamW', 'Adam']
-
-# Others
-reps = 5
 
 
 def objective(trial):
-    d = []
+    d = {}
 
     # Global hyperparams
     d['n_layers'] = trial.suggest_int('n_layers', min_layers, max_layers)
     d['lr'] = trial.suggest_float('lr', min_lr, max_lr, log=True)
-    d['beta_1'] = trial.suggest_float(
-        'beta_1', min_beta_1, max_beta_1, log=True)
-    d['beta_2'] = trial.suggest_float(
-        'beta_2', min_beta_2, max_beta_2, log=True)
-    d['optimizer'] = trial.suggest_categorical('optimizer', opt)
+    d['optimizer'] = 'AdamW'
     d['weight_decay'] = \
         trial.suggest_float('weight_decay', min_weight_decay,
                             max_weight_decay, log=True)
     d['average_pool_kernel_size'] = \
-        trial.suggest_int('avg_kernel_size', min_avg_w, max_avg_w, step=2)
+        trial.suggest_int('avg_kernel_size', min_avg_w, max_avg_w)
 
     # Local hyperparams
     d['blocks'] = []
@@ -70,49 +58,63 @@ def objective(trial):
         d['blocks'].append(
             trial.suggest_int(f'layer{layer_idx}_nblocks', min_b, max_b))
         d['n_channels'].append(
-            trial.suggest_int(f'layer{layer_idx}_nhidden', min_h, max_h))
+            trial.suggest_int(f'layer{layer_idx}_nhidden',
+                              min_h, max_h, log=True))
         d['kernel_sizes'].append(
-            trial.suggest_int(f'layer{layer_idx}_kernel_size', min_w, max_w, step=2))
+            trial.suggest_int(f'layer{layer_idx}_kernel_size',
+                              min_w, max_w, step=2))
         d['skip_kernel_sizes'].append(
-            trial.suggest_int(f'layer{layer_idx}_skip_kernel_size', min_w, max_w, step=2))
+            trial.suggest_int(f'layer{layer_idx}_skip_kernel_size',
+                              min_w, max_w, step=2))
 
-    # Load optimization policy
-    # TODO: Change this once we finish finding policy
-    train_transform = A.load("./data_augmentation/output//policy.json")
-    test_transform = None  # TODO: Add this
+    # Instantiate model
+    model = ResNet(**d)
+
+    # If model parameter count > 5M, return a bad value
+    if sum(p.numel() for p in model.parameters() if p.requires_grad) > 5e6:
+        return -1
+
+    # Load augmentation policy
+    train_transform = A.load(
+        "./data_augmentation/outputs/2022-03-07/20-57-53/policy/latest.json")
+    test_transform = A.load(
+        "./data_augmentation/outputs/2022-03-07/20-57-53/policy/latest.json")
+    # TODO: What do we use as a test policy?
 
     # Create dataset
-    train_dataset = CIFAR10(root='~/data/CIFAR10',
-                            transform=train_transform, train=True)
-    test_dataset = CIFAR10(root='~/data/CIFAR10',
-                           tansform=test_transform, train=False)
+    train_dataset = Cifar10SearchDataset(root='~/data/CIFAR10',
+                                         transform=train_transform, train=True)
+    test_dataset = Cifar10SearchDataset(root='~/data/CIFAR10',
+                                        transform=test_transform, train=False)
 
     # Split train dataset into train and test. Use 0.2 as ratio.
     val_len = len(train_dataset) // 5
     train_dataset, val_dataset = random_split(
         train_dataset, [len(train_dataset)-val_len, val_len])
+    train_loader = DataLoader(
+        train_dataset,
+        batch_size=1024, num_workers=32, shuffle=True, pin_memory=True)
+    val_loader = DataLoader(
+        val_dataset,
+        batch_size=1024, num_workers=8, shuffle=False, pin_memory=True)
+    test_loader = DataLoader(
+        test_dataset,
+        batch_size=1024, num_workers=8, shuffle=False, pin_memory=True)
 
-    # Evaluate model performance
-    test_accs = []
-    for _ in range(reps):
-        # Instantiate model
-        model = ResNet(d)
+    # Run train and val
+    wandb_logger = WandbLogger(project="DLProject1")
+    trainer = pl.Trainer(
+        logger=wandb_logger,
+        checkpoint_callback=False,
+        max_epochs=50,
+        gpus=1,
+        callbacks=[PyTorchLightningPruningCallback(trial, monitor="val_acc")],
+    )
+    trainer.fit(model, train_dataloader=train_loader,
+                val_dataloaders=val_loader)
+    trainer.test(model, test_dataloaders=test_loader)
 
-        # Instatiate optimizer
-        optimizer = getattr(torch.optim, d['optimizer'])(
-            model.parameters(),
-            lr=d['lr'], betas=(d['beta_1'], d['beta_2']),
-            weight_decay=d['weight_decay'])
-
-        # If model parameter count > 5M, return a bad value
-        if sum(p.numel() for p in model.parameters() if p.requires_grad) > 5e6:
-            return -1
-
-        # Run train and test
-        test_accs.append(train_and_test(model, optimizer,
-                         train_dataset, val_dataset, test_dataset))
-
-    return sum(test_accs)/len(test_accs)
+    return trainer.callback_metrics["val_acc"].item()
 
 
 if __name__ == "__main__":
@@ -124,3 +126,6 @@ if __name__ == "__main__":
         load_if_exists=True,
         pruner=optuna.pruners.HyperbandPruner())
     study.optimize(objective, n_trials=1000, timeout=60000)
+
+    # Find the best model, and test it
+    test_transform = None  # TODO: Add this
--- project1_model.py
+++ project1_model.py
@@ -1,22 +1,32 @@
+import pytorch_lightning as pl
+from torchmetrics.functional import accuracy
+
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+
+
 class BasicBlock(nn.Module):
 
-    def __init__(self, in_planes, planes, stride=1):
+    def __init__(
+            self, in_planes, planes, stride=1,
+            kernel_size=3, skip_kernel_size=1):
         super(BasicBlock, self).__init__()
         self.conv1 = nn.Conv2d(
-            in_planes, planes, kernel_size=3, stride=stride, padding=1, bias=False)
+            in_planes, planes, kernel_size=kernel_size, stride=stride,
+            padding=kernel_size//2, bias=False)
         self.bn1 = nn.BatchNorm2d(planes)
-        self.conv2 = nn.Conv2d(planes, planes, kernel_size=3,
-                               stride=1, padding=1, bias=False)
+        self.conv2 = nn.Conv2d(planes, planes, kernel_size=kernel_size,
+                               stride=1, padding=kernel_size//2, bias=False)
         self.bn2 = nn.BatchNorm2d(planes)
 
         self.shortcut = nn.Sequential()
         if stride != 1 or in_planes != planes:
             self.shortcut = nn.Sequential(
                 nn.Conv2d(in_planes, planes,
-                          kernel_size=1, stride=stride, bias=False),
+                          kernel_size=skip_kernel_size,
+                          padding=skip_kernel_size//2,
+                          stride=stride, bias=False),
                 nn.BatchNorm2d(planes)
             )
 
@@ -28,39 +38,79 @@ class BasicBlock(nn.Module):
         return out
 
 
-
-class ResNet(nn.Module):
-    def __init__(self, block, num_blocks, num_classes=10):
+class ResNet(pl.LightningModule):
+    def __init__(self, **d):
         super(ResNet, self).__init__()
-        self.in_planes = 64
 
-        self.conv1 = nn.Conv2d(3, 64, kernel_size=3,
+        self.save_hyperparameters()
+
+        block = BasicBlock
+        num_classes = 10
+        self.d = d
+        self.in_planes = d['n_channels'][0]
+
+        self.conv1 = nn.Conv2d(3, self.in_planes, kernel_size=3,
                                stride=1, padding=1, bias=False)
-        self.bn1 = nn.BatchNorm2d(64)
-        self.layer1 = self._make_layer(block, 64, num_blocks[0], stride=1)
-        self.layer2 = self._make_layer(block, 128, num_blocks[1], stride=2)
-        self.layer3 = self._make_layer(block, 256, num_blocks[2], stride=2)
-        self.layer4 = self._make_layer(block, 512, num_blocks[3], stride=2)
-        self.linear = nn.Linear(512, num_classes)
+        self.bn1 = nn.BatchNorm2d(self.in_planes)
 
-    def _make_layer(self, block, planes, num_blocks, stride):
+        self.layers = nn.ModuleList()
+        for layer in range(d['n_layers']):
+            self.layers.append(
+                self._make_layer(
+                    block, d['n_channels']
+                    [layer], d['blocks'][layer],
+                    kernel_size=d['kernel_sizes'][layer],
+                    skip_kernel_size=d['skip_kernel_sizes'][layer],
+                    stride=1 if layer == 1 else 2))
+
+        self.linear = nn.Linear(d['n_channels'][-1], num_classes)
+
+    def configure_optimizers(self):
+        return torch.optim.AdamW(
+            self.parameters(),
+            lr=self.d['lr'],
+            weight_decay=self.d['weight_decay']
+        )
+
+    def _make_layer(self, block, planes, num_blocks, stride, **kwargs):
         strides = [stride] + [1]*(num_blocks-1)
         layers = []
         for stride in strides:
-            layers.append(block(self.in_planes, planes, stride))
+            layers.append(block(self.in_planes, planes, stride, **kwargs))
             self.in_planes = planes
         return nn.Sequential(*layers)
 
     def forward(self, x):
         out = F.relu(self.bn1(self.conv1(x)))
-        out = self.layer1(out)
-        out = self.layer2(out)
-        out = self.layer3(out)
-        out = self.layer4(out)
+        for layer in self.layers:
+            out = layer(out)
         out = F.avg_pool2d(out, 4)
         out = out.view(out.size(0), -1)
         out = self.linear(out)
-        return out
+        return F.log_softmax(out, dim=-1)
 
-def project1_model():
-    return ResNet(BasicBlock, [2, 2, 2, 2])
+    # Training step, eval step, and val step from
+    # https://pytorch-lightning.readthedocs.io/en/stable/notebooks/lightning_examples/cifar10-baseline.html
+    def training_step(self, batch, batch_idx):
+        x, y = batch
+        logits = self(x)
+        loss = F.nll_loss(logits, y)
+        self.log("train_loss", loss)
+        return loss
+
+    def evaluate(self, batch, stage=None):
+        x, y = batch
+        logits = self(x)
+        loss = F.nll_loss(logits, y)
+        preds = torch.argmax(logits, dim=1)
+        acc = accuracy(preds, y)
+
+        if stage:
+            self.log(f"{stage}_loss", loss, prog_bar=True)
+            self.log(f"{stage}_acc", acc, prog_bar=True)
+
+    def validation_step(self, batch, batch_idx):
+        self.evaluate(batch, "val")
+
+    def test_step(self, batch, batch_idx):
+        self.evaluate(batch, "test")
